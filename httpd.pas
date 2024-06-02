9b62435efb81c3562d5f45b05bd61c0d17e9a864
vhanla <vhanla@gmail.com>
2024-06-01 22:15:52 -0500

    Apache HTTPD handler via process handler interface

--- httpd.pas
+++ httpd.pas
@@ -8,14 +8,45 @@ unit httpd;
 interface
 
 uses
-  Winapi.Windows, System.SysUtils, System.Classes, processhandler, Settings;
+  Winapi.Windows, System.SysUtils, System.Classes, processhandler, Settings, DosCommand;
 
 type
-  TApacheManager = class
+  TApacheManager = class(DosCommand.TDosCommand, IProcess)
   private
-
+    fProcess: TDosCommand;
+    function IsRunning: Boolean;
+    function GetName: string;
+    function GetFullPath: string;
+    function GetPID: Integer;
+    property Running: Boolean read IsRunning;
+    property Name: string read GetName;
+    property FullPath: string read GetFullPath;
+    property PID: Integer read GetPID;
   end;
 
 implementation
+
+
+{ TApacheManager }
+
+function TApacheManager.GetFullPath: string;
+begin
+
+end;
+
+function TApacheManager.GetName: string;
+begin
+
+end;
+
+function TApacheManager.GetPID: Integer;
+begin
+
+end;
+
+function TApacheManager.IsRunning: Boolean;
+begin
+
+end;
 
 end.
